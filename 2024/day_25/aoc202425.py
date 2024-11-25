0037e1db88e831147a1c0708727681c614b748fe
Ben Currie <benjamin.currie@fundingcircle.com>
2024-11-25 18:34:23 +0000

    add templates

---
+++ 2024/day_25/aoc202425.py
@@ -0,0 +1,31 @@
+import pathlib
+import sys
+from aocd.models import Puzzle
+
+
+def parse(puzzle_input):
+    """Parse input."""
+    return puzzle_input.split()
+
+def part_one(data):
+    """Solve part 1."""
+
+def part_two(data):
+    """Solve part 2."""
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input."""
+    data = parse(puzzle_input)
+    solution_one = part_one(data)
+    solution_two = part_two(data)
+
+    return solution_one, solution_two
+
+if __name__ == "__main__":
+    for path in sys.argv[1:]:
+        puzzle = Puzzle(year=2024, day=25)
+        puzzle_input = puzzle.input_data
+        with open(path, 'w') as input_file:
+            input_file.write(puzzle_input)
+        solutions = solve(puzzle_input)
+        print(f"Part One: {solutions[0]}\nPart Two: {solutions[1]}")
