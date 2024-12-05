ddd7fe78675e5624bdcea8dcf5d090449836f1f4
Ben Currie <benjamin.currie@fundingcircle.com>
2024-12-05 21:11:41 +0000

    day 5 - part one and two

--- 2024/day_05/aoc202405.py
+++ 2024/day_05/aoc202405.py
@@ -1,17 +1,74 @@
+from collections import defaultdict
 from aocd.models import Puzzle
 
 
 def parse(puzzle_input):
-    """Parse input."""
-    return puzzle_input.split()
+    """Parse input into ordering rules and page numbers."""
+    sections = puzzle_input.split("\n\n")
+    ordering_rules = defaultdict(list)
+    page_numbers = []
+
+    for section in sections:
+        if "|" in section:
+            for line in section.split("\n"):
+                key, value = line.split("|")
+                ordering_rules[key].append(value)
+        else:
+            page_numbers.append([row.split(",") for row in section.split("\n")])
+
+    return ordering_rules, page_numbers[0]
 
 
 def part_one(data):
     """Solve part 1."""
+    rules = data[0]
+    pages = data[1]
+    total = 0
+    for page in pages:
+        if all_rules_pass(page, rules):
+            total += int(page[len(page) // 2])
+    return total
 
 
 def part_two(data):
     """Solve part 2."""
+    rules = data[0]
+    pages = data[1]
+    total = 0
+    for page in pages:
+        if not all_rules_pass(page, rules):
+            correctly_ordered_page = correct_ordering(page, rules)
+            total += int(correctly_ordered_page[len(page) // 2])
+    return total
+
+
+def all_rules_pass(page, rules):
+    all_rules_passed = True
+    for index, page_number in enumerate(page):
+        valid_pages_to_be_ahead = rules[page_number]
+        if all_rules_passed:
+            if not all(str(x) in valid_pages_to_be_ahead for x in page[index + 1 :]):
+                all_rules_passed = False
+    return all_rules_passed
+
+
+def correct_ordering(page, ordering_rules_dict):
+    """Swap the values over based on the rules until no more swaps are needed"""
+    new_page = []
+    correct_order = False
+    while not correct_order:
+        correct_order = True
+        for number_index, number in enumerate(page):
+            if number_index == len(page) - 1:
+                continue
+            if number in ordering_rules_dict[page[number_index + 1]]:
+                # swap the current number with the next one
+                page[number_index + 1], page[number_index] = (
+                    page[number_index],
+                    page[number_index + 1],
+                )
+                correct_order = False
+    return page
 
 
 def solve(puzzle_input):
--- 2024/day_05/test_aoc202405.py
+++ 2024/day_05/test_aoc202405.py
@@ -17,22 +17,26 @@ def example_two():
     return aoc.parse(puzzle_input)
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_parse_example_one(example_one):
     """Test that input is parsed properly."""
-    assert example_one == ...
+    assert example_one[1] == [
+        ["75", "47", "61", "53", "29"],
+        ["97", "61", "53", "29", "13"],
+        ["75", "29", "13"],
+        ["75", "97", "47", "61", "53"],
+        ["61", "13", "29"],
+        ["97", "13", "75", "29", "47"],
+    ]
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part_one_example_one(example_one):
     """Test part 1 on example input."""
-    assert aoc.part_one(example_one) == ...
+    assert aoc.part_one(example_one) == 143
 
 
-@pytest.mark.skip(reason="Not implemented")
 def test_part_two_example_one(example_one):
     """Test part 2 on first example input."""
-    assert aoc.part_two(example_one) == ...
+    assert aoc.part_two(example_one) == 123
 
 
 @pytest.mark.skip(reason="Not implemented")
