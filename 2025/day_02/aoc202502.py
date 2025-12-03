5338304c57eebab9535f68ee3428d4566b46777d
Ben Currie <benjamin.currie@fundingcircle.com>
2025-12-03 00:13:12 +0000

    Day two - part one and two

--- 2025/day_02/aoc202502.py
+++ 2025/day_02/aoc202502.py
@@ -1,16 +1,38 @@
+import re
 from aocd.models import Puzzle
 
 
 def parse(puzzle_input):
     """Parse input."""
+    return [tuple(map(int, line.split("-"))) for line in puzzle_input.split(",")]
+
+
+def is_repeat(y, half_length):
+    return y[:half_length] == y[-half_length:]
 
 
 def part_one(data):
     """Solve part 1."""
+    total = 0
+    codes = [x for start, end in data for x in range(start, end + 1)]
+    for code in codes:
+        y = str(code)
+        q, r = divmod(len(y), 2)
+        if r == 0 and is_repeat(y, q):
+            total += code
+    return total
 
 
 def part_two(data):
     """Solve part 2."""
+    total = 0
+    codes = [x for start, end in data for x in range(start, end + 1)]
+    for code in codes:
+        y = str(code)
+        q, r = divmod(len(y), 2)
+        if (r == 0 and is_repeat(y, q)) or bool(re.fullmatch(r"(.+)\1+", y)):
+            total += code
+    return total
 
 
 def solve(puzzle_input):
--- 2025/day_02/test_aoc202502.py
+++ 2025/day_02/test_aoc202502.py
@@ -19,17 +19,37 @@ def example_two():
 
 def test_parse_example_one(example_one):
     """Test that input is parsed properly."""
-    assert example_one == ...
+    assert example_one == [
+        (11, 22),
+        (95, 115),
+        (998, 1012),
+        (1188511880, 1188511890),
+        (222220, 222224),
+        (1698522, 1698528),
+        (446443, 446449),
+        (38593856, 38593862),
+        (565653, 565659),
+        (824824821, 824824827),
+        (2121212118, 2121212124),
+    ]
+
+
+def test_is_repeat():
+    """Test is_repeat function returns correct value"""
+    assert aoc.is_repeat("1212", 2) is True
+    assert aoc.is_repeat("123123", 3) is True
+    assert aoc.is_repeat("1234", 2) is False
+    assert aoc.is_repeat("12345", 2) is False
 
 
 def test_part_one_example_one(example_one):
     """Test part 1 on example input."""
-    assert aoc.part_one(example_one) == ...
+    assert aoc.part_one(example_one) == 1227775554
 
 
 def test_part_two_example_one(example_one):
     """Test part 2 on first example input."""
-    assert aoc.part_two(example_one) == ...
+    assert aoc.part_two(example_one) == 4174379265
 
 
 @pytest.mark.skip(reason="Not needed")
